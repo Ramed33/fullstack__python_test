331acd29547201b135562d28e5c06d180cd16bd5
Ramed33 <149710707+Ramed33@users.noreply.github.com>
2024-11-11 17:57:40 -0600

    Subiendo el aprendizaje del modulo 50

--- src/config/urls.py
+++ src/config/urls.py
@@ -14,8 +14,6 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
-from django.conf import settings
 from django.contrib import admin
 from django.urls import include
 from django.urls import path
@@ -23,10 +21,7 @@ from django.urls import path
 urlpatterns = [
     path("up/", include("up.urls")),
     path("", include("pages.urls")),
+    path("analytics/", include("analytics.urls")),
     path("admin/", admin.site.urls),
+    path("__debug__/", include("debug_toolbar.urls")),
 ]
-if not settings.TESTING:
-    urlpatterns = [
-        *urlpatterns,
-        path("__debug__/", include("debug_toolbar.urls")),
-    ]
